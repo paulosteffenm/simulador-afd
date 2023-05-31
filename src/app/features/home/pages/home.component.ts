7f8fb40b7186a9c84c4912c001c803fc41112091
paulo <paulosteffenm@gmail.com>
2023-05-30 21:03:20 -0300

    feat: new form steps

--- src/app/features/home/pages/home.component.ts
+++ src/app/features/home/pages/home.component.ts
@@ -7,25 +7,35 @@ import { FormBuilder, Validators } from '@angular/forms';
 })
 export class HomeComponent {
 
-  public formGroup = this.formBuilder.group({
+  public formGroupAddNodes = this.formBuilder.group({
     nodeName: ['', Validators.required]
   });
 
-  public nodes: Array<string> = [];
+  public formGroupSelectFirstNode = this.formBuilder.group({
+    nodes: [[], Validators.required]
+  });
+
+  public get nodes():any{
+    return [];
+  }
 
   constructor(private formBuilder: FormBuilder) { }
 
   public addNewNode(): void {
 
-    const isNotEmpty = this.formGroup.value.nodeName && this.formGroup.value.nodeName?.length > 0;
+    const isNotEmpty = this.formGroupAddNodes.value.nodeName && this.formGroupAddNodes.value.nodeName?.length > 0;
 
-    const newItem = this.nodes.find((node) => node === this.formGroup.value.nodeName);
+    const newItem = this.nodes.find((node) => node === this.formGroupAddNodes.value.nodeName);
 
     if (isNotEmpty && !newItem) {
-      this.nodes.push(this.formGroup.value.nodeName!);
+      this.nodes.push(this.formGroupAddNodes.value.nodeName!);
     }
 
-    this.formGroup.controls['nodeName'].setValue('');
+    this.formGroupAddNodes.controls['nodeName'].setValue('');
 
+  }
+
+  public removeNode(nodeName: string): void {
+    this.nodes = this.nodes.filter((node) => node !== nodeName);
   }
 }
